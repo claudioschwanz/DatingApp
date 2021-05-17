cb4ad0d7d591e3b1622873b47e6f75faa7cdc2f9
claudio schwanz <claudioschwanz@gmail.com>
2021-05-17 20:37:14 -0300

    Adding Loading Indicator and Using Service to store data

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -24,6 +24,7 @@ import { ServerErrorComponent } from './Errors/server-error/server-error.compone
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 
 
 @NgModule({
@@ -53,7 +54,8 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
-    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true}
   ],
   bootstrap: [AppComponent]
 })
